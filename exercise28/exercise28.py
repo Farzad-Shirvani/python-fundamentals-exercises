268fba71b33510aa08a03261dcebfbed0b5ddfb5
Farzad <farzadshirvani003@gmail.com>
2025-10-01 22:09:37 +0330

    Update exercise28.py

--- exercise28/exercise28.py
+++ exercise28/exercise28.py
@@ -10,17 +10,11 @@ def gcd_2(n_1,n_2):
     return n_2    
     
 def gcd_5(m_1, m_2, m_3, m_4, m_5):
-    numbers = [m_1, m_2, m_3, m_4, m_5]
-    sorted_numbers = sorted(numbers)
-    x = gcd_2(sorted_numbers[4],sorted_numbers[3])
-    numbers_1 = [sorted_numbers[0], sorted_numbers[1], sorted_numbers[2],x]
-    sorted_numbers_1 = sorted(numbers_1)
-    y = gcd_2(sorted_numbers_1[3], sorted_numbers_1[2])
-    numbers_2 = [sorted_numbers_1[0], sorted_numbers_1[1],y]
-    sorted_numbers_2 = sorted(numbers_2)
-    z = gcd_2(sorted_numbers_2[2],sorted_numbers_2[1])
-    numbers_3 = [sorted_numbers_2[0], z]
-    sorted_numbers_3 = sorted(numbers_3)
-    return gcd_2(sorted_numbers_3[1],sorted_numbers_3[0])
+    x = gcd_2(m_1, m_2)
+    x = gcd_2(x, m_3)
+    x = gcd_2(x, m_4)
+    x = gcd_2(x, m_5)
+    return x    
     
-print(gcd_5(a, b, c, d, e))  
+print(gcd_5(a, b, c, d, e))    
+    
