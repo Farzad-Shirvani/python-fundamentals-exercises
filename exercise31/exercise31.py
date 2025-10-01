1f6abc6d7652a0c1998cb3b7b0473ff90e4e6f71
Farzad <farzadshirvani003@gmail.com>
2025-10-01 23:40:40 +0330

    Add files via upload

---
+++ exercise31/exercise31.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+
+def is_prime(a, b):
+    for i in range(a, b+1):
+        if i == 1:
+            continue
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            print(i) 
+            
+is_prime(a, b)
